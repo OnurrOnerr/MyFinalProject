cce48b5f4328101c53492ea30eb75f935b9ba63b
Onur Öner <onuroner_@hotmail.com>
2021-02-24 14:41:54 +0300

    24 Şubat

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //Ientity imlement eden class bir veri tabanı tablosudur.
     public interface IEntity
