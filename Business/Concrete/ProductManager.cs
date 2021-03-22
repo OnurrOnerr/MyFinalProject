aad012c934f05177f1d48841feb94024e2d99043
Onur Öner <onuroner_@hotmail.com>
2021-03-22 15:50:09 +0300

    22.03.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspect.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -31,9 +32,9 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
+        //Claim
 
-
-
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
 
         public IResult Add(Product product)
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,6 +16,12 @@ namespace Business.Constants
         public static string ProductCountOdCategoryError="Bir Kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExceeded = "Kategori Limiti aşılıdığı için yeni ürün eklnemiyor";
-
+        public static string AuthorizationDenied = "Yetkiniz Yok";
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+        public static string PasswordError = "Parola hatalı";
+        public static string SuccessfulLogin = "Tam yetkili giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "erişim şifresi oluşturuldu";
     }
 }
